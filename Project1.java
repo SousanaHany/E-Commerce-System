f6d042a707a20249f7409e25e3e60c310ac77f7d
SousanaHany <167803075+SousanaHany@users.noreply.github.com>
2024-04-22 23:32:27 +0200

    Project1.java

--- Project1.java
+++ Project1.java
@@ -4,6 +4,7 @@
  */
 package project1;
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 public class Project1 {
 
     public static void main(String[] args) {
@@ -12,23 +13,24 @@ public class Project1 {
         ElectronicProduct electronicProduct = new ElectronicProduct( "smartphone",1,1,"Samsung", 599.9f );
         ClothingProduct clothingproduct = new ClothingProduct( "Medium", "Cotton", 2 , "T-shirt", 19.99f);
         BookProduct bookProduct = new BookProduct("O'Reilly", "X Publications", 3, "OOP", 39.99f);
-        System.out.println("Welcome to the E-Commerce System! ");
-        System.out.println("Enter your ID:");
-        int customerId = Math.abs(scanner.nextInt());
-        System.out.println("Enter your name:");
-        String name = scanner.next();
-        System.out.println("Enter your address:");
-        String address = scanner.next();
+        
+        JOptionPane.showMessageDialog(null, "Welcome to the E-Commerce System! ");
+       int customerId = Integer.parseInt(JOptionPane.showInputDialog("Enter your id:"));
+       JOptionPane.showMessageDialog(null, "you Id "+ customerId );
+       String name = JOptionPane.showInputDialog("Enter your name:");
+       JOptionPane.showMessageDialog(null, "Hello "+name);
+        String address = JOptionPane.showInputDialog("Enter your address:");
+        JOptionPane.showMessageDialog(null, "your address "+address);
 
         Customer customer = new Customer(customerId, name, address);
 
-        System.out.println("How many products do you want to add to your cart?");
-        int numProducts = Math.abs(scanner.nextInt());
-        Cart cart = new Cart(customer.getCustomerId(), numProducts);
+         int nomproducts= Integer.parseInt(JOptionPane.showInputDialog("Enter num of products "));
+              Cart cart=new Cart(customerId,nomproducts);
 
-        for (int i = 0; i < numProducts; i++) {
-            System.out.println("Which product would you like to add 1- smartphone , 2- T-shirt , 3-Oop");
-            int productId = Math.abs(scanner.nextInt());
+        for (int i = 0; i < nomproducts; i++) {
+           JOptionPane.showMessageDialog(null, "choose product to add");
+           JOptionPane.showMessageDialog(null, "1- smartphone, 2- T-shirt, 3- Oop");
+         int productId = Integer.parseInt(JOptionPane.showInputDialog("Enter Your choise"));
             Product product;
             switch (productId) {
                 case 1:
@@ -41,25 +43,26 @@ public class Project1 {
                     product = bookProduct;
                     break;
                 default:
-                    System.out.println("Invalid product ID. Please try again.");
+                    JOptionPane.showMessageDialog(null, "Invalid product ID. Please try again.");
                     i--;
                     continue;
             }
             cart.addproduct(product);
         }
 
-        System.out.println("whould you like to place the order ? 1- yes 2- no");
-        int choice = scanner.nextInt();
+         JOptionPane.showMessageDialog(null, " whould you like to place the order ? 1-yes/2-no");
+        int choice = Integer.parseInt(JOptionPane.showInputDialog("Enter your choice: "));
         if (choice == 1) {
             Order order = new Order(customer.getCustomerId(), 1 );
             cart.placeorder(order, cart);
             order.printOrderInfo();
         } else {
-            System.out.println("Thank you for using our E-Commerce system!");
+            JOptionPane.showMessageDialog(null, "Thank you for using our E-Commerce system!");
         }
 
     }
 }
+    
     
  
 
